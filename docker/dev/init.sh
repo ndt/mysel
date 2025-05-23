05b4bd5639aefdddc15bc9fbc23ad5a1aee54ab3
Helge Wiethoff <helge.wiethoff@thga.de>
2025-05-23 09:14:36 +0200

    Replicator-user in env.example hinzugefügt und healthstatus bei freeradius per nc (weniger debug auth)

--- docker/dev/init.sh
+++ docker/dev/init.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-source .env.dev
-
 if [ ! -f .env.dev ]; then
   cp env.example .env.dev
   echo "Created .env.dev file from env.example template"
 else
   echo ".env file already exists, skipping creation"
 fi
-  
+
 # openLDAP
 set -e
 ./openldap/generate_bootstrap.sh
