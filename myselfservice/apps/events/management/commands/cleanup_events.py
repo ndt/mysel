843b2b07b2e2f0119cf7473a439f940d136e7d26
Nicolas Nieswandt <nicolas.nieswandt@gmail.com>
2025-07-02 19:32:00 +0200

    Fix für doppelte STATIC_URL und nicht existierender Ordner "staticfiles"

--- myselfservice/apps/events/management/commands/cleanup_events.py
+++ myselfservice/apps/events/management/commands/cleanup_events.py
@@ -1,4 +1,4 @@
-# apps/events/management/commands/cleanup_events.py 
+# apps/events/management/commands/cleanup_events.py
 from django.core.management.base import BaseCommand
 from django.utils import timezone
 from apps.events.models import Event
