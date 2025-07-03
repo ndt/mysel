36b5cac6d66c58097484552bc195fbc352c7e050
Helge Wiethoff <helge.wiethoff@thga.de>
2025-07-03 15:14:43 +0200

    updated requirements and keep staticfiles folder

--- myselfservice/config/settings.py
+++ myselfservice/config/settings.py
@@ -182,9 +182,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
